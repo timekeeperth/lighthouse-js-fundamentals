8327fc5a1fc855c05f7a544286bbd500465e241e
a.yz <a.yz@outlook.com>
2018-07-31 05:23:07 +0000

    commiting

---
+++ odd.js
@@ -0,0 +1,18 @@
+//function isOdd(num) {
+//  return % 2 === 1
+// }
+
+// console.log("3 is odd: " + isOdd(3));
+// console.log("8 is odd: " + isOdd(8));
+
+//
+
+var total = 0
+
+var limit = 10
+
+function isOdd(num) {
+  return num % 2 === 1
+}
+
+console.log(isOdd(10));
