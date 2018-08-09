23d843a97306abe2f71674faba03ec15ae899083
a.yz <a.yz@outlook.com>
2018-08-09 08:32:16 +0000

    Adds looping exercises

---
+++ areaCalculators.js
@@ -0,0 +1,28 @@
+//Define 3 functions to calculate the areas areas of 3 different shapes: rectangles, triangles
+and circles.
+
+//The function names and parameters:
+
+//calculateRectangleArea(length, width)
+//calculateTriangleArea(base, height)
+//calculateCircleArea(radius)
+
+//The parameters to the functions will always be numbers.
+
+//rectangle: langth * width
+//triangle: base * height / 2
+//circle: pi * radius**2
+
+//If any of the parameters are negative, the function should return undefined.
+
+//To access pi: Math.PI.
+
+var numOne = 5
+
+var numTwo = 6
+
+function calculateRectangleArea(numOne, numTwo) {
+  return (numbOne * numbTwo)
+};
+
+console.log(calculateRectangleArea(numOne, numTwo));
